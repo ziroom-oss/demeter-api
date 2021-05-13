87c4124a29cacdbe3d343edf01cfd211b25982e9
daijinru <jeocat@163.com>
2021-05-13 18:19:13 +0800

    :construction: 图谱开发中

---
+++ src/main/java/com/ziroom/tech/demeterapi/controller/GraphController.java
@@ -0,0 +1,71 @@
+package com.ziroom.tech.demeterapi.controller;
+
+import com.ziroom.tech.demeterapi.dao.entity.GraphAreaSkill;
+import com.ziroom.tech.demeterapi.dao.entity.GraphSkill;
+import com.ziroom.tech.demeterapi.dao.entity.GraphSubSkillTask;
+import com.ziroom.tech.demeterapi.po.dto.Resp;
+import com.ziroom.tech.demeterapi.po.dto.req.Graph.GraphSkillReq;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Api(tags = "任务相关")
+@Slf4j
+@RestController
+@RequestMapping("api/graph")
+public class GraphController {
+
+    /**
+     * 获取图谱列表接口
+     */
+    @GetMapping("/list")
+    public Resp<List<GraphSkill>> listGraphSkill() {
+        return Resp.success();
+    }
+
+    /**
+     * 获取技能领域列表
+     * 技能领域同时包含它的技能
+     */
+    @GetMapping("/area/list")
+    public Resp<List<GraphAreaSkill>> listGraphArea() {
+        return Resp.success();
+    }
+
+    /**
+     * 获取子技能列表
+     */
+    @GetMapping("/subSkill/list")
+    public Resp<List<GraphSubSkillTask>> listSubSkill() {
+        return Resp.success();
+    }
+
+    /**
+     * 创建和修改技能图谱
+     */
+    @PostMapping("/")
+    public Resp<Object> createGraph(GraphSkillReq graphSkillReq) {
+        return Resp.success();
+    }
+
+    /**
+     * 创建和修改技能领域
+     */
+    @PostMapping("/area")
+    public Resp<Object> createArea() {
+        return Resp.success();
+    }
+
+    /**
+     * 创建和修改子技能
+     */
+    @PostMapping("/subSkill")
+    public Resp<Object> createSubSkill() {
+        return Resp.success();
+    }
+}
--- src/main/java/com/ziroom/tech/demeterapi/po/dto/req/Graph/GraphSkillReq.java
+++ src/main/java/com/ziroom/tech/demeterapi/po/dto/req/Graph/GraphSkillReq.java
@@ -1,21 +1,44 @@
 package com.ziroom.tech.demeterapi.po.dto.req.Graph;
 
+import com.google.common.base.Preconditions;
+import com.ziroom.tech.demeterapi.dao.entity.GraphSkill;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
 
 @Data
 @ApiModel("新增和编辑技能图谱")
 public class GraphSkillReq {
     @ApiModelProperty("主键id，也是图谱编号")
-    private Integer id;
+    private Long id;
 
     @ApiModelProperty("图谱名称")
     private String graphName;
 
     @ApiModelProperty("启用状态")
-    private Byte enable;
+    private Integer enable;
 
     @ApiModelProperty("职务")
     private Integer position;
+
+    public GraphSkill getEntity() {
+        GraphSkill graphSkill = new GraphSkill();
+        if (Objects.nonNull(id)) {
+            graphSkill.setId(id);
+        }
+        validate();
+        graphSkill.setGraphName(graphName);
+        graphSkill.setEnable(enable);
+        graphSkill.setPosition(position);
+        return graphSkill;
+    }
+
+    public void validate() {
+        Preconditions.checkArgument(StringUtils.isNotBlank(graphName), "图谱名称不能为空");
+        Preconditions.checkArgument(Objects.nonNull(enable), "启用状态不能为空");
+        Preconditions.checkArgument(Objects.nonNull(position), "职务不能为空");
+    }
 }
